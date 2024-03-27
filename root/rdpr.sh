e569db3150f3e5d848c80c36a1b727b36a1768e6
Zsombi1089 <116731984+Zsombi1089@users.noreply.github.com>
2024-03-27 10:12:51 +0100

    Create rdpr.sh

---
+++ root/rdpr.sh
@@ -0,0 +1,14 @@
+clear
+apt update && apt upgrade -y && apt install screen wget unzip nano curl git tar -y
+echo -e "Downloading... Please Wait"
+apt update && apt upgrade -y
+export SUDO_FORCE_REMOVE=yes
+apt remove sudo -y
+apt install lxde -y
+apt install xrdp -y
+echo "lxsession -s LXDE -e LXDE" >> /etc/xrdp/startwm.sh
+echo -e "Select RDP Port"
+read selectedPort
+sed -i "s/port=3389/port=$selectedPort/g" /etc/xrdp/xrdp.ini
+service xrdp restart
+echo -e "RDP Created And Started on 3389 Port"
