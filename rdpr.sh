91503e48a1036960bd419a3db21d8c72bf1eb613
Zsombi1089 <116731984+Zsombi1089@users.noreply.github.com>
2024-03-27 17:22:38 +0100

    Update rdpr.sh

--- rdpr.sh
+++ rdpr.sh
@@ -2,8 +2,6 @@ clear
 apt update && apt upgrade -y && apt install screen wget unzip nano curl git tar -y
 echo -e "Downloading... Please Wait"
 apt update && apt upgrade -y
-export SUDO_FORCE_REMOVE=yes
-apt remove sudo -y
 apt install lxde -y
 apt install xrdp -y
 echo "lxsession -s LXDE -e LXDE" >> /etc/xrdp/startwm.sh
